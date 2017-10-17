9fae5b8e5a0fed74a3dc2784f2e53cd66924f2cd
zegui7 <jose.gcp.almeida@gmail.com>
2017-10-17 09:17:22 +0100

    all

--- file_manager.py
+++ file_manager.py
@@ -4,13 +4,33 @@ client_filename = "clients.txt"
 transaction_filename = "transactions.txt"
 
 def add_client(client_name):
-    pass
+    o = open("clients.txt",a)
+    o.write("\n" + client_name)
+    o.close()
       
 def add_transaction(debtor, creditor, amount):
-    pass
+    new_list = [str(x).strip() for x in [debtor,creditor,amount]]
+    to_write = ' '.join(new_list)
+    o = open("transactions.txt",a)
+    o.write(to_write)
+    o.close()
         
 def get_clients():
-    pass
+    if os.ispath("clients.txt"):
+	o = open("clients.txt")
+	lines = o.readlines()
+	return lines
+    else:
+	return []
 
 def get_transactions():
-    pass
+    o = open("transactions.txt")
+    lines = o.readlines()
+    o.close()
+    new_list = [[],[],[]]
+    for line in lines:
+	line.split()
+	new_list[0].append(line[0])
+	new_list[1].append(line[1])
+	new_list[2].append(line[2])
+    return new_list
--- functionalities.py
+++ functionalities.py
@@ -1,13 +1,36 @@
 import file_manager as fM
 
 def say_hi():
-    pass
+    print("Hi!")
 
 def new_client():
-    pass
+    new_cl = raw_input("Insert the name of the new client:")
+    prev_cl = fM.get_clients()
+    if new_cl not in prev_cl:
+	fM.add_client(new_cl)
     
 def new_transaction():
-    pass
-    
+    deb = raw_input("Insert the name of the debtor:")
+    cred = raw_input("Insert the name of the creditor:")
+    amo = raw_input("Insert the name of the amount:")
+    cl = fM.get_clients()
+    if (deb in cl) and (cred in cl):
+	fM.add_transaction(deb,cred,amo)
+    elif deb not in cl:
+	print("Debtor does not exist!")
+    elif cred not in cl:
+	print("Creditor does not exist!")
+    elif deb not in cl:
+	print("Debtor and creditor do not exist!")
+
 def look_credit():
-    pass
+    cl = fM.get_clients()
+    print(cl)
+    curr = raw_input("Please login:")
+    trans = fM.get_transactions()
+    curr_cash = 0
+    for tran in trains:
+	if curr in tran[0]:
+	    curr_cash = curr_cash - tran[2]
+	if curr in tran[1]:
+	    curr_cash += tran[2]
